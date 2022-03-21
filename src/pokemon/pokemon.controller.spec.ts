653ee04653a6d96fcf7046534c091b6af1888f8f
Davide Ramaglietta <davide.ramaglietta@gmail.com>
2022-03-21 18:29:34 +0100

    add unit tests for GET /pokemon/<name> endpoint

--- src/pokemon/pokemon.controller.spec.ts
+++ src/pokemon/pokemon.controller.spec.ts
@@ -1,18 +1,55 @@
+import { HttpModule, HttpService } from '@nestjs/axios';
 import { Test, TestingModule } from '@nestjs/testing';
 import { PokemonController } from './pokemon.controller';
+import { PokemonService } from './pokemon.service';
 
 describe('PokemonController', () => {
   let controller: PokemonController;
+  let service: PokemonService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [HttpModule],
       controllers: [PokemonController],
+      providers: [PokemonService],
     }).compile();
 
     controller = module.get<PokemonController>(PokemonController);
+    service = module.get<PokemonService>(PokemonService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('findOne', () => {
+    it('should return the pokemon object', async () => {
+      const expected = {
+        name: 'wormadam',
+        description:
+          'When the bulb on\nits back grows\nlarge, it appears\fto lose the\nability to stand\non its hind legs',
+        habitat: 'water',
+        isLegendary: false,
+      };
+      const serviceResponse = {
+        name: 'wormadam',
+        is_legendary: false,
+        habitat: { name: 'water' },
+        flavor_text_entries: [
+          {
+            flavor_text:
+              'When the bulb on\nits back grows\nlarge, it appears\fto lose the\nability to stand\non its hind legs',
+            language: {
+              name: 'en',
+            },
+          },
+        ],
+      };
+      jest
+        .spyOn(service, 'findOne')
+        .mockImplementation(() => Promise.resolve(serviceResponse));
+
+      expect(await controller.findOne('pokemon')).toStrictEqual(expected);
+    });
   });
 });
--- src/pokemon/pokemon.service.spec.ts
+++ src/pokemon/pokemon.service.spec.ts
@@ -1,18 +1,51 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { HttpModule, HttpService } from '@nestjs/axios';
 import { PokemonService } from './pokemon.service';
 
 describe('PokemonService', () => {
   let service: PokemonService;
+  let httpService: HttpService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [HttpModule],
       providers: [PokemonService],
     }).compile();
 
     service = module.get<PokemonService>(PokemonService);
+    httpService = module.get<HttpService>(HttpService);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('findOne', () => {
+    it('should return the pokemon response', async () => {
+      const response = {
+        data: {
+          name: 'wormadam',
+          is_legendary: false,
+          habitat: { name: 'water' },
+          flavor_text_entries: [
+            {
+              flavor_text:
+                'When the bulb on\nits back grows\nlarge, it appears\fto lose the\nability to stand\non its hind legs',
+              language: {
+                name: 'en',
+              },
+            },
+          ],
+        },
+      };
+      jest.spyOn(httpService, 'get').mockImplementation(
+        () =>
+          ({
+            toPromise: () => Promise.resolve(response),
+          } as any),
+      );
+
+      expect(await service.findOne('pokemon')).toStrictEqual(response.data);
+    });
   });
 });
